38b72e5e4d4d59bf2cb02bc6ad9145b7b82434c9
Min <alstjq9292@naver.com>
2017-06-07 13:20:11 +0900

    약수구하기 변수 선언

---
+++ Algorithm/Algorithm/divisor.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int num;
+int array[100];
+int c;
+int d;
+int mok;
+int ngm;
+int i;
+
+int main(int argc, char* argv[]) {
+
+	cout << "약수구하기" << endl;
+	cout << "숫자를 입력하세요 :: ";
+	cin >> num;
+
+	return 0;
+}
